8ec59e1c6117fc34ff99f18d9fd7e0b3b6fd6103
schmaenjael <manuelmayer04@gmail.com>
2023-04-21 20:44:18 +0200

    feat(landingpage): add typewriter component

---
+++ src/components/Typewriter.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef } from 'react';
+import { useUtilities } from '~/services';
+
+interface TypeWriterProps {
+  content: string[];
+  typeSpeed?: number;
+  awaitDuration?: number;
+  repeat?: boolean;
+}
+
+const Typewriter = ({ content, typeSpeed = 300, awaitDuration = 3000, repeat = false }: TypeWriterProps) => {
+  const type = useRef<'write' | 'delete'>('write');
+  const [text, setText] = useState<string>('');
+  const { sleep } = useUtilities();
+  const index = useRef(-1);
+
+  const writer = {
+    ['write']: () => setText(content[index.current].slice(0, text.length + 1)),
+    ['delete']: () => setText(content[index.current].slice(0, text.length - 1)),
+  };
+
+  useEffect(() => {
+    if (repeat && index.current === content.length - 1 && text === '' && type.current === 'delete') index.current = -1;
+    if (text === '') { index.current += 1; type.current = 'write'; } // prettier-ignore
+    if (text === content[index.current]) type.current = 'delete';
+
+    sleep(text === content[index.current] || text === '' ? awaitDuration : typeSpeed).then(writer[type.current]);
+  }, [text]);
+
+  return (
+    <>
+      {text} <span className="text-primary blinking">▐</span>
+    </>
+  );
+};
+
+export default Typewriter;
