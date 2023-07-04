8fd9e5aae5aab9a1334a60d18c8ea5e2d199f508
schmaenjael <mayer.manuel@protonmail.ch>
2023-07-04 23:27:06 +0200

    feat(error): add nightsky canvas component for landingpage

--- src/components/elements/Nightsky.tsx
+++ src/components/elements/Nightsky.tsx
@@ -1,29 +1,25 @@
 import { useEffect, useRef } from 'react';
 import { useNightsky, NightSkyConfiguration } from '~/hooks/useNightsky';
 
-class Star {
-  constructor(public x: number, public y: number, public radius: number, public color: `#${string}`) {}
-
-  draw(ctx: CanvasRenderingContext2D) {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, Math.random() * this.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-  }
+interface NightSkyProps extends NightSkyConfiguration {
+  className?: string;
 }
 
-const Nightsky = () => {
+const Nightsky = (props: NightSkyProps) => {
   const canvas = useRef<HTMLCanvasElement>(null);
   const { initialize } = useNightsky();
 
-  const config: NightSkyConfiguration = {
-    starCount: 100,
-  };
+  const config: NightSkyConfiguration = { ...props };
 
   useEffect(() => initialize(canvas, config), []);
 
-  return <canvas id="canvas-nightsky" className="h-full w-full border-4 border-red-400" ref={canvas}></canvas>;
+  return (
+    <canvas
+      id="canvas-nightsky"
+      className={`pointer-events-none absolute z-[1] h-full w-full ${props.className}`}
+      ref={canvas}
+    />
+  );
 };
 
 export default Nightsky;
