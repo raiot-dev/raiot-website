8ae972533dbd5f322aa18d0d32f406e485b8e65e
schmaenjael <mayer.manuel@protonmail.ch>
2023-08-05 23:22:36 +0200

    feat(error): move models and config to respective folders

---
+++ src/config/visuals/visuals.ts
@@ -0,0 +1,12 @@
+import { NightSkyConfiguration } from '~/models/visuals';
+
+export const NightskyConfig: NightSkyConfiguration = {
+  color: '#ffffff',
+  minRadius: 0.75,
+  maxRadius: 2,
+  minOpacity: 50,
+  maxOpacity: 100,
+  minSpeed: 0.005,
+  maxSpeed: 0.125,
+  starCount: 250,
+};
